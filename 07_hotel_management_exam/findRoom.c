92a3de0ea3f4097afdafc7ee19a8ea06725fbed8
Andrea Barrano <53945231+AndreaBroke97@users.noreply.github.com>
2026-08-31 15:03:41 +0200

    codice sistemato

--- 07_hotel_management_exam/findRoom.c
+++ 07_hotel_management_exam/findRoom.c
@@ -1,29 +1,43 @@
 #include <stdio.h>
 #include <string.h>
 
-//#define MAX[100]
+#define MAX 100
 
-//struct Room {
-//    numberRoom[MAX];
-//}
-
-
-int findRoom(struct Room archive, int count) {
-
-
+struct Room {
     int numberRoom;
-        printf("insert the number of the room for searching: ");
-        scanf("%d", &numberRoom);
+};
 
-            for(int i = 0; i < count - 1; i++){
-
-                if (numberRoom == archive[i].numberRoom) {
-                    return i;   
-            }
-
-         
+int findRoom(struct Room archive[], int count) {
+    int numberRoom;
+    printf("insert the number of the room for searching: ");
+    scanf("%d", &numberRoom);
+    for (int i = 0; i < count; i++) {
+        if (numberRoom == archive[i].numberRoom) {
+            return i;
         }
-        
+    }
     return -1;
-}   
+}
 
+int main(void) {
+    struct Room archive[MAX];
+    int count;
+
+    printf("how many rooms do you want to enter: ");
+    scanf("%d", &count);
+
+    for (int i = 0; i < count; i++) {
+        printf("Enter number for room %d: ", i);
+        scanf("%d", &archive[i].numberRoom);
+    }
+
+    int index = findRoom(archive, count);
+
+    if (index == -1) {
+        printf("Room not found!\n");
+    } else {
+        printf("Room found at position: %d\n", index);
+    }
+
+    return 0;
+}
