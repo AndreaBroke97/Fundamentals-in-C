329ff37fb6797f39c11cd840b9902c0b6e86337b
Andrea Barrano <53945231+AndreaBroke97@users.noreply.github.com>
2026-08-31 15:22:41 +0200

    codice sistemato

--- 07_hotel_management_exam/esame.c
+++ 07_hotel_management_exam/esame.c
@@ -1,27 +1,47 @@
 #include <stdio.h>
 #include <string.h>
 
-#define TIPOLOGY_OF_ROOM[10]
-#define STATE_OF_ROOM[10]
-#define NAME_VARIABLE[30]
+#define MAX 100
+#define TIPOLOGY_LENGTH 10
+#define STATE_LENGTH 10
+#define NAME_LENGTH 30
 
 struct rooms {
     int numberOfRoom;
+    char tipologyOfRoom[TIPOLOGY_LENGTH];
     int maxCapience;
     float priceForNight;
     int numOfNightBooked;
-    char nameVariable[30];
+    char stateOfRoom[STATE_LENGTH];
+    char nameVariable[NAME_LENGTH];
 };
 
-struct rooms arrayRooms[100];
+struct rooms arrayRooms[MAX];
 int count = 0;
 
+// Funzione di appoggio: cerca una camera dal numero e restituisce
+// la sua posizione nell'array, oppure -1 se non esiste.
+int findRoomIndex(int number) {
+    for (int i = 0; i < count; i++) {
+        if (arrayRooms[i].numberOfRoom == number) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+// 1: inserimento di una nuova camera
 void addRoom() {
+    if (count == MAX) {
+        printf("Archivio pieno: impossibile aggiungere altre camere.\n");
+        return;
+    }
+
     printf("Insert the number of the room: ");
     scanf("%d", &arrayRooms[count].numberOfRoom);
-    
-    printf("Insert the tipology of the room: ");
-    scanf("%s", arrayRooms[count].tipology_of_room);
+
+    printf("Insert the tipology of the room (singola/doppia/tripla/suite): ");
+    scanf("%s", arrayRooms[count].tipologyOfRoom);
 
     printf("insert the max capience of the room: ");
     scanf("%d", &arrayRooms[count].maxCapience);
@@ -32,94 +52,313 @@ void addRoom() {
     printf("insert the number of night booked: ");
     scanf("%d", &arrayRooms[count].numOfNightBooked);
 
-    printf("insert the state of the room: ");
+    printf("insert the state of the room (libera/occupata): ");
     scanf("%s", arrayRooms[count].stateOfRoom);
 
-    printf("insert the name of the customer: ");    
+    printf("insert the name of the customer: ");
     scanf("%s", arrayRooms[count].nameVariable);
-        numRooms++;
-};
 
+    count++;
+}
+
+// 2: visualizzazione completa di tutte le camere
 void showAllRooms() {
-    for(int i = 0; i < numRooms; i++) {
-        printf("Room number: %d\n", arrayRooms[i].numberOfRoom);
+    if (count == 0) {
+        printf("Nessuna camera presente in archivio.\n");
+        return;
+    }
 
-        printf("");
+    for (int i = 0; i < count; i++) {
+        printf("Room number: %d\n", arrayRooms[i].numberOfRoom);
+        printf("Tipology: %s\n", arrayRooms[i].tipologyOfRoom);
+        printf("Max capience: %d\n", arrayRooms[i].maxCapience);
+        printf("Price for night: %.2f\n", arrayRooms[i].priceForNight);
+        printf("Nights booked: %d\n", arrayRooms[i].numOfNightBooked);
+        printf("State: %s\n", arrayRooms[i].stateOfRoom);
+        printf("Customer name: %s\n", arrayRooms[i].nameVariable);
+        printf("--------------------\n");
     }
 }
 
+// 3: cercare una camera mediante il numero identificativo
+void searchRoom() {
+    int number;
+    printf("Insert the number of the room to search: ");
+    scanf("%d", &number);
 
+    int index = findRoomIndex(number);
+
+    if (index == -1) {
+        printf("Camera inesistente\n");
+        return;
+    }
+
+    printf("Room number: %d\n", arrayRooms[index].numberOfRoom);
+    printf("Tipology: %s\n", arrayRooms[index].tipologyOfRoom);
+    printf("Max capience: %d\n", arrayRooms[index].maxCapience);
+    printf("Price for night: %.2f\n", arrayRooms[index].priceForNight);
+    printf("Nights booked: %d\n", arrayRooms[index].numOfNightBooked);
+    printf("State: %s\n", arrayRooms[index].stateOfRoom);
+    printf("Customer name: %s\n", arrayRooms[index].nameVariable);
+}
+
+// 4: aggiornamento del prezzo per notte
+void updatePrice() {
+    int number;
+    printf("Insert the number of the room: ");
+    scanf("%d", &number);
+
+    int index = findRoomIndex(number);
+
+    if (index == -1) {
+        printf("Camera inesistente\n");
+        return;
+    }
+
+    printf("Insert the new price for night: ");
+    scanf("%f", &arrayRooms[index].priceForNight);
+    printf("Prezzo aggiornato.\n");
+}
+
+// 5: aggiornamento del numero di notti prenotate
+void updateNights() {
+    int number;
+    printf("Insert the number of the room: ");
+    scanf("%d", &number);
+
+    int index = findRoomIndex(number);
+
+    if (index == -1) {
+        printf("Camera inesistente\n");
+        return;
+    }
+
+    printf("Insert the new number of nights booked: ");
+    scanf("%d", &arrayRooms[index].numOfNightBooked);
+    printf("Notti prenotate aggiornate.\n");
+}
+
+// 6: modifica dello stato della camera
+void updateState() {
+    int number;
+    printf("Insert the number of the room: ");
+    scanf("%d", &number);
+
+    int index = findRoomIndex(number);
+
+    if (index == -1) {
+        printf("Camera inesistente\n");
+        return;
+    }
+
+    printf("Insert the new state (libera/occupata): ");
+    scanf("%s", arrayRooms[index].stateOfRoom);
+    printf("Stato aggiornato.\n");
+}
+
+// 7: registrazione di una prenotazione (check-in)
+void checkIn() {
+    int number;
+    printf("Insert the number of the room to book: ");
+    scanf("%d", &number);
+
+    int index = findRoomIndex(number);
+
+    if (index == -1) {
+        printf("Camera inesistente\n");
+        return;
+    }
+
+    if (strcmp(arrayRooms[index].stateOfRoom, "occupata") == 0) {
+        printf("La camera e' gia' occupata.\n");
+        return;
+    }
+
+    printf("Insert the name of the customer: ");
+    scanf("%s", arrayRooms[index].nameVariable);
+
+    printf("Insert the number of nights booked: ");
+    scanf("%d", &arrayRooms[index].numOfNightBooked);
+
+    strcpy(arrayRooms[index].stateOfRoom, "occupata");
+    printf("Prenotazione registrata.\n");
+}
+
+// 8: registrazione del check-out
+void checkOut() {
+    int number;
+    printf("Insert the number of the room for check-out: ");
+    scanf("%d", &number);
+
+    int index = findRoomIndex(number);
+
+    if (index == -1) {
+        printf("Camera inesistente\n");
+        return;
+    }
+
+    strcpy(arrayRooms[index].nameVariable, "");
+    arrayRooms[index].numOfNightBooked = 0;
+    strcpy(arrayRooms[index].stateOfRoom, "libera");
+    printf("Check-out registrato.\n");
+}
+
+// 9: calcolare l'importo totale della prenotazione (prezzo x notti)
+void totalBookingAmount() {
+    int number;
+    printf("Insert the number of the room: ");
+    scanf("%d", &number);
+
+    int index = findRoomIndex(number);
+
+    if (index == -1) {
+        printf("Camera inesistente\n");
+        return;
+    }
+
+    float total = arrayRooms[index].priceForNight * arrayRooms[index].numOfNightBooked;
+    printf("Importo totale della prenotazione: %.2f\n", total);
+}
+
+// 10 - elaborazione 1: incasso teorico complessivo delle camere occupate
+void totalRevenue() {
+    float total = 0;
+    for (int i = 0; i < count; i++) {
+        if (strcmp(arrayRooms[i].stateOfRoom, "occupata") == 0) {
+            total += arrayRooms[i].priceForNight * arrayRooms[i].numOfNightBooked;
+        }
+    }
+    printf("Incasso teorico complessivo (camere occupate): %.2f\n", total);
+}
+
+// 10 - elaborazione 2: conteggio delle camere per tipologia
+void countByType() {
+    int single = 0, doubleR = 0, triple = 0, suite = 0;
+
+    for (int i = 0; i < count; i++) {
+        if (strcmp(arrayRooms[i].tipologyOfRoom, "singola") == 0) {
+            single++;
+        } else if (strcmp(arrayRooms[i].tipologyOfRoom, "doppia") == 0) {
+            doubleR++;
+        } else if (strcmp(arrayRooms[i].tipologyOfRoom, "tripla") == 0) {
+            triple++;
+        } else if (strcmp(arrayRooms[i].tipologyOfRoom, "suite") == 0) {
+            suite++;
+        }
+    }
+
+    printf("Singole: %d\n", single);
+    printf("Doppie: %d\n", doubleR);
+    printf("Triple: %d\n", triple);
+    printf("Suite: %d\n", suite);
+}
+
+// 10 - elaborazione 3: camera con l'importo totale piu' elevato
+void highestAmount() {
+    float max = 0;
+    int roomNumber = -1;
+
+    for (int i = 0; i < count; i++) {
+        float amount = arrayRooms[i].priceForNight * arrayRooms[i].numOfNightBooked;
+        if (amount > max) {
+            max = amount;
+            roomNumber = arrayRooms[i].numberOfRoom;
+        }
+    }
+
+    if (roomNumber == -1) {
+        printf("Nessuna camera con un importo calcolabile.\n");
+    } else {
+        printf("La camera con l'importo piu' alto e' la %d con %.2f\n", roomNumber, max);
+    }
+}
+
+// 10 - elaborazione 4: prezzo medio per notte delle camere registrate
+void averagePrice() {
+    if (count == 0) {
+        printf("Nessuna camera registrata.\n");
+        return;
+    }
+
+    float total = 0;
+    for (int i = 0; i < count; i++) {
+        total += arrayRooms[i].priceForNight;
+    }
+
+    printf("Prezzo medio per notte: %.2f\n", total / count);
+}
+
+// 10: sotto-menu delle elaborazioni
+void elaborations() {
+    int choice;
+
+    do {
+        printf("\n--- ELABORAZIONI ---\n");
+        printf("1: Incasso teorico complessivo\n");
+        printf("2: Conteggio camere per tipologia\n");
+        printf("3: Camera con importo piu' alto\n");
+        printf("4: Prezzo medio per notte\n");
+        printf("0: Torna al menu principale\n");
+        printf("Scelta: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1: totalRevenue(); break;
+            case 2: countByType(); break;
+            case 3: highestAmount(); break;
+            case 4: averagePrice(); break;
+            case 0: break;
+            default: printf("Scelta non valida.\n"); break;
+        }
+    } while (choice != 0);
+}
 
 void showmenu() {
     int choice;
 
     do {
         printf("\n-------MENU-------\n");
-        printf("1: Number of rooms\n");
+        printf("1: Add room\n");
         printf("2: Show all rooms\n");
-        printf("3: Search rooms\n");
-        printf("4: Price for night\n");
-        printf("5: Type of rooms\n");
-        printf("6: State of the rooms\n");
-        printf("7: Add room\n");
-        printf("8:\n");
-        printf("9:\n");
-        printf("10:\n");
-        printf("11:\n");
-        printf("12: Exit\n");
+        printf("3: Search room\n");
+        printf("4: Update price for night\n");
+        printf("5: Update booked nights\n");
+        printf("6: Update room state\n");
+        printf("7: Register booking (check-in)\n");
+        printf("8: Register check-out\n");
+        printf("9: Total booking amount\n");
+        printf("10: Elaborations\n");
+        printf("11: Exit\n");
 
         printf("insert your choice: ");
         scanf("%d", &choice);
 
         switch (choice) {
-            case 1: 
-            printf("Number of rooms: %d\n", numRooms); 
-            break;
-            case 2: 
-            printf("show all rooms: %d\n", numRooms); 
-            break;
-            case 3: 
-            printf("s\n"); 
-            break;
-            case 4: 
-            printf("todo\n"); 
-            break;
-            case 5: 
-            printf("todo\n"); 
-            break;
-            case 6: 
-            printf("todo\n"); 
-            break;
-            case 7: 
-            printf("todo\n"); 
-            break;
-            case 8: 
-            printf("todo\n"); 
-            break;
-            case 9: 
-            printf("todo\n");
-            break;
-            case 10: 
-            printf("todo\n"); 
-            break;
-            case 11: 
-            printf("todo\n"); 
-            break;
-            case 12: 
-            printf("Exit\n"); 
-            break;
-            default: 
-            printf("Invalid Number\n"); 
-            break;
+            case 1: addRoom(); break;
+            case 2: showAllRooms(); break;
+            case 3: searchRoom(); break;
+            case 4: updatePrice(); break;
+            case 5: updateNights(); break;
+            case 6: updateState(); break;
+            case 7: checkIn(); break;
+            case 8: checkOut(); break;
+            case 9: totalBookingAmount(); break;
+            case 10: elaborations(); break;
+            case 11: printf("Exit\n"); break;
+            default: printf("Invalid Number\n"); break;
         }
 
-    } while(choice != 12);
+    } while (choice != 11);
 }
 
 int main(void) {
     showmenu();
     return 0;
 }
+
+
+
+//ESAME 
 
 //1: inserimento di una nuova camera
 
@@ -200,3 +439,4 @@ e divide il risultato per il numero totale di camere
 //11: Uscita
 
 //////////////////////////////////////////
+
